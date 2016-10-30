79ac73885d7a9791b077896dd99bb10375d83267
Laijiaxiang <934613503@qq.com>
2016-10-30 10:05:15 +0800

    修改类，删除一句代码

--- JavaTest/src/com/laijiaxiang/test/JavaTest.java
+++ JavaTest/src/com/laijiaxiang/test/JavaTest.java
@@ -3,7 +3,6 @@ package com.laijiaxiang.test;
 public class JavaTest {
 	public static void main(String[] args) {
 		System.out.println("This is a test!");
-		System.out.println("shikong!");
 		System.out.println("shikong2!");
 		System.out.println("Laijiaxiang");
 	}
