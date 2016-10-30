c9d8ffeabad88f25a274ebd156f3c52c632ef28c
shikong <1770735527@qq.com>
2016-10-30 09:34:16 +0800

    shikong1

--- JavaTest/src/com/laijiaxiang/test/JavaTest.java
+++ JavaTest/src/com/laijiaxiang/test/JavaTest.java
@@ -3,5 +3,7 @@ package com.laijiaxiang.test;
 public class JavaTest {
 	public static void main(String[] args) {
 		System.out.println("This is a test!");
+		System.out.println("shikong!");
+		
 	}
 }
