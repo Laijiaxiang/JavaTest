ee29076605dde7a7f063c2e795718d9f7b7ec475
shikong <1770735527@qq.com>
2016-10-30 09:49:28 +0800

    shikogn4

--- JavaTest/src/com/laijiaxiang/test/JavaTest.java
+++ JavaTest/src/com/laijiaxiang/test/JavaTest.java
@@ -4,6 +4,6 @@ public class JavaTest {
 	public static void main(String[] args) {
 		System.out.println("This is a test!");
 		System.out.println("shikong!");
-		
+		System.out.println("shikong2!");
 	}
 }
